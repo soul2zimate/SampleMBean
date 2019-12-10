460d95d1946c035aa2da42e10eff6b6de65445da
Chao Wang <chaowan@redhat.com>
2019-12-10 15:44:11 +0800

    EJB injection in mbean

--- src/main/java/sample/mbean/ConfigService.java
+++ src/main/java/sample/mbean/ConfigService.java
@@ -2,14 +2,22 @@ package sample.mbean;
 
 import sample.ejb.Hello;
 import javax.ejb.EJB;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
 import javax.inject.Inject;
 
-public class ConfigService implements ConfigServiceMBean {
+@Singleton
+@Startup
+public class ConfigService extends AbstractComponentMBean implements ConfigServiceMBean {
 
     // work well with @EJB, but we cannot use @Inject to inject EJB session bean in .sar archive.
 //    @EJB
     @Inject
     Hello helloBean;
+
+    public ConfigService() {
+        super("quickstarts");
+    }
 
     @Override
     public void start() {
--- src/main/java/sample/mbean/ConfigServiceMBean.java
+++ src/main/java/sample/mbean/ConfigServiceMBean.java
@@ -1,5 +1,8 @@
 package sample.mbean;
 
+import javax.management.MXBean;
+
+@MXBean
 public interface ConfigServiceMBean {
     void start();
     void stop();
